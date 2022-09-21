28d8d35d3c569cf81b570ce1ab29f5c4d5c56b65
Matt McKnett <mmcknett@gmail.com>
2022-09-21 14:41:26 -0700

    feat: Adding and updating documents in firestore
    The only issue with this update algorithm is that it is impossible to
    find and delete all of the buildings that are *not* in an update.

--- src/import.js
+++ src/import.js
@@ -1,64 +1,98 @@
-const buildings = require('./buildings.json');
-const firebase = require('firebase');
-require('firebase/firestore');
+import * as fs from 'node:fs';
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import * as dotenv from 'dotenv';
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DB,
-  projectId: process.env.REACT_APP_PID,
-  storageBucket: process.env.REACT_APP_SB,
-  messagingSenderId: process.env.REACT_APP_SID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MID
-};
+dotenv.config();
 
-firebase.initializeApp(firebaseConfig);
+const DESTINATION_COLLECTION = 'buildings-2022-01';
+const DATABASE_TO_IMPORT = './db/buildings-2022-01-31.json';
+
+const buildings = JSON.parse(fs.readFileSync(DATABASE_TO_IMPORT)).slice(0,2);
+
+const firebaseApp = initializeApp({
+  credential: applicationDefault(),
+  databaseURL: "https://mfte-simple-92c08-default-rtdb.firebaseio.com"
+});
+
 let countWritten = 0;
 let countUpdated = 0;
-const db = firebase.firestore();
+const db = getFirestore(firebaseApp);
 
-buildings.forEach(function(obj) {
-  db.collection("buildings")
-  .add({
-    buildingName: obj.buildingName,
-    phone: obj.phone,
-    phone2: obj.phone2,
-    residentialTargetedArea: obj.residentialTargetedArea,
-    totalRestrictedUnits: obj.totalRestrictedUnits,
-    sedu: obj.sedu,
-    studioUnits: obj.studioUnits,
-    oneBedroomUnits: obj.oneBedroomUnits,
-    twoBedroomUnits: obj.twoBedroomUnits,
-    threePlusBedroomUnits: obj.threePlusBedroomUnits,
-    urlForBuilding: obj.urlForBuilding,
-    lat: obj.lat,
-    lng: obj.lng,
-    streetNum: obj.streetNum,
-    street: obj.street,
-    city: obj.city,
-    state: obj.state,
-    zip: obj.zip
-  }).then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-    countWritten += 1;
-    console.log("Count written: ", countWritten);
 
-    const currentBuilding = db.collection("buildings").doc(docRef.id);
-
-    return currentBuilding.update({
-      buildingID: docRef.id
-    })
-    .then(() => {
-      console.log("Document successfully updated with id: ", docRef.id);
-      countUpdated += 1;
-      console.log("Count updated: ", countUpdated);
-    })
-    .catch((error) => {
-      console.error("Error updating document: ", error);
-    });
-  })
-  .catch(function(error) {
-    console.error("Error adding document: ", error);
-  });
+// Import each building in the JSON file
+const extractFields = (bldg, revision = 0) => ({
+  buildingName: bldg.buildingName,
+  phone: bldg.phone,
+  phone2: bldg.phone2,
+  residentialTargetedArea: bldg.residentialTargetedArea,
+  totalRestrictedUnits: bldg.totalRestrictedUnits,
+  sedu: bldg.sedu,
+  studioUnits: bldg.studioUnits,
+  oneBedroomUnits: bldg.oneBedroomUnits,
+  twoBedroomUnits: bldg.twoBedroomUnits,
+  threePlusBedroomUnits: bldg.threePlusBedroomUnits,
+  urlForBuilding: bldg.urlForBuilding,
+  lat: bldg.lat,
+  lng: bldg.lng,
+  streetNum: bldg.streetNum,
+  street: bldg.street,
+  city: bldg.city,
+  state: bldg.state,
+  zip: bldg.zip,
+  revision
 });
+
+for await (const bldg of buildings) {
+  const buildingCollection = db.collection(DESTINATION_COLLECTION);
+  const buildingsWithThisName = await buildingCollection.where('buildingName', '==', bldg.buildingName).get();
+
+  // If the building exists...
+  if (!buildingsWithThisName.empty) {
+    // Warn if there are duplicates
+    if (buildingsWithThisName.size > 1) {
+      console.error(`WARNING: multiple buildings exist in the DB with the name "${bldg.buildingName}"`);
+    }
+    const bldgToUpdate = buildingsWithThisName.docs.at(0);
+
+    // The building exists; check if it needs to be updated w/ a revision.
+    const currentBuildingData = bldgToUpdate.data();
+    const newBuildingData = extractFields(bldg, currentBuildingData.revision);
+
+    if (JSON.stringify(currentBuildingData) === JSON.stringify(newBuildingData)) {
+      // No changes; no revision needed.
+      continue;
+    }
+
+    // Something changed. Increment the revision and set the new building data.
+    newBuildingData.revision += 1;
+    await bldgToUpdate.ref.set(newBuildingData);
+    console.log(`Successfully updated doc with id ${bldgToUpdate.ref.id}, revision ${nextRevision}`);
+    countUpdated += 1;
+    continue;
+  }
+
+  // Otherwise, if the building does not exist...
+  let docRef
+  try {
+    docRef = await buildingCollection.add(extractFields(bldg));
+  } catch(error) {
+    console.error("Error adding document: ", error);
+  }
+
+  console.log("Document added with ID: ", docRef.id);
+  countWritten += 1;
+
+  const currentBuilding = buildingCollection.doc(docRef.id);
+
+  try {
+    await currentBuilding.update({ buildingID: docRef.id });
+    console.log("Document successfully updated with id: ", docRef.id);
+    countUpdated += 1;
+  } catch (error) {
+    console.error("Error updating document: ", error);
+  }
+}
+
+console.log("Count added: ", countWritten);
+console.log("Count updated: ", countUpdated);
