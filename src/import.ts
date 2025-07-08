07ef249b5497d6395f9ef8600655fbf78a79ef80
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2025-07-08 12:54:01 -0400

    buildings data update

--- src/import.ts
+++ src/import.ts
@@ -1,5 +1,5 @@
 // README Step 5: Change this to the updated json
-import buildings from "./BuildingJSONs/buildings_06_3_2025.json" assert { type: "json" };
+import buildings from "./BuildingJSONs/buildings_columbia_city_studios_2025_07_08.json" assert { type: "json" };
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, Timestamp } from "firebase/firestore";
 import { OriginalFields } from "./types_and_interfaces/OriginalFields";
